2df9e34e79b70853cf94dbbe164261a71a6afa93
Johnexzy <obajohn75@gmail.com>
2022-12-12 16:40:45 +0100

    wip: addded sendmesaage trigers on active tabs

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,14 +1,70 @@
 const generateCompletionAction = async (info) => {
   try {
+    sendMessage("generating...");
     const { selectionText } = info;
     const basePromptPrefix = `
 	Write me a detailed table of contents for a blog post with the title below.
 
 	Title:
 	`;
+    const baseCompletion = await generate(
+      `${basePromptPrefix}${selectionText}`
+    );
+    const secondPrompt = `
+      Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+      
+      Title: ${selectionText}
+      
+      Table of Contents: ${baseCompletion.text}
+      
+      Blog Post:
+      `;
+
+    // Call your second prompt
+    const secondPromptCompletion = await generate(secondPrompt);
+    // Let's see what we get!
+    console.log(secondPromptCompletion.text);
+    sendMessage(secondPromptCompletion.text);
   } catch (error) {
     console.log(error);
+    sendMessage(error.toString());
   }
+};
+
+const getKey = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(["openai-key"], (result) => {
+      if (result["openai-key"]) {
+        const decodedKey = atob(result["openai-key"]);
+        resolve(decodedKey);
+      }
+    });
+  });
+};
+
+const generate = async (prompt) => {
+  // Get your API key from storage
+  const key = await getKey();
+  const url = "https://api.openai.com/v1/completions";
+
+  // Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 1250,
+      temperature: 0.7,
+    }),
+  });
+
+  // Select the top choice and send back
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
 };
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -18,5 +74,20 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+const sendMessage = (content) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0].id;
+
+    chrome.tabs.sendMessage(
+      activeTab,
+      { message: "inject", content },
+      (response) => {
+        if (response.status === "failed") {
+          console.log("injection failed.");
+        }
+      }
+    );
+  });
+};
 // Add listener
 chrome.contextMenus.onClicked.addListener(generateCompletionAction);
