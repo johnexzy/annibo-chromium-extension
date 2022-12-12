c116d9ab4e2d9e8bcb58511f3f8b05f477e542c0
Johnexzy <obajohn75@gmail.com>
2022-12-12 16:17:28 +0100

    added contextMenu triggers

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,8 +1,20 @@
+const generateCompletionAction = async (info) => {
+  try {
+    const { selectionText } = info;
+    const basePromptPrefix = `
+	Write me a detailed table of contents for a blog post with the title below.
+
+	Title:
+	`;
+  } catch (error) {
+    console.log(error);
+  }
+};
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: 'context-run',
-    title: 'Generate blog post',
-    contexts: ['selection'],
+    id: "context-run",
+    title: "Generate blog post",
+    contexts: ["selection"],
   });
 });
 
